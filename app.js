ad3cb921171949de4a24adee271c2100c5f7772a
ulmj7 <ulmjgt@gmail.com>
2025-09-25 02:06:06 +0800

    shoog shiddeg bolgson

--- app.js
+++ app.js
@@ -11,16 +11,22 @@ var roundScore = 0;
 
 //Shoonii ali talaaraa buusniig hadgalah huvisagch heregtei, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej  ugnu.
 
-var dice = Math.floor(Math.random()*6)+1;
-
-// window.document.querySelector('#score-0').textContent=dice
-
-// window.document.querySelector('#score-1').innerHTML='<em>Yes!</em>'
+var diceNumber = Math.floor(Math.random()*6)+1;
 
 // Program ehlehed beltgey
-window.document.querySelector('#score-0').textContent='0'
-window.document.querySelector('#score-1').textContent='0'
-document.querySelector('.dice').style.display='none'
-document.querySelector('#current-0').textContent='0'
-document.querySelector('#current-1').textContent='0'
-console.log('Shoo: ' + dice)
+document.getElementById('score-0').textContent='0'
+document.getElementById('score-1').textContent='0'
+
+var diceDom = document.querySelector('.dice')
+
+diceDom.style.display='none'
+document.getElementById('current-0').textContent='0'
+document.getElementById('current-1').textContent='0'
+
+document.querySelector('.btn-roll').addEventListener('click',function shooShid(){
+    var diceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.style.display='block'
+    diceDom.src = 'dice-' + diceNumber + '.png'
+})
+
+
