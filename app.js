fb00a6c0cbc2b22d3174a97831ffb89acbf8fb39
ulmj7 <ulmjgt@gmail.com>
2025-09-25 01:27:31 +0800

    ehnii orlt

---
+++ app.js
@@ -0,0 +1,26 @@
+// Toglogchiin eeljiig hadgalah huvisagch, negdugeer toglogchiig 0, hoyrdugaar toglogchiig neg gej temdegley
+var activePlayer = 1;
+
+// Toglogchdiin tsugluulsan onoog hadgalah huvisagch
+
+var scores =[0,0]
+
+// Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huvisagch
+
+var roundScore = 0;
+
+//Shoonii ali talaaraa buusniig hadgalah huvisagch heregtei, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej  ugnu.
+
+var dice = Math.floor(Math.random()*6)+1;
+
+// window.document.querySelector('#score-0').textContent=dice
+
+// window.document.querySelector('#score-1').innerHTML='<em>Yes!</em>'
+
+// Program ehlehed beltgey
+window.document.querySelector('#score-0').textContent='0'
+window.document.querySelector('#score-1').textContent='0'
+document.querySelector('.dice').style.display='none'
+document.querySelector('#current-0').textContent='0'
+document.querySelector('#current-1').textContent='0'
+console.log('Shoo: ' + dice)
